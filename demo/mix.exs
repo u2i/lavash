2d343267ba996bb7bfc2ea1b6c199852fa8271e7
Tom Clarke <tom@u2i.com>
2026-03-12 15:35:28 +0200

    Update dependency versions in demo app

--- demo/mix.exs
+++ demo/mix.exs
@@ -61,7 +61,7 @@ defmodule Demo.MixProject do
       {:telemetry_metrics, "~> 1.0"},
       {:telemetry_poller, "~> 1.0"},
       {:jason, "~> 1.2"},
-      {:dns_cluster, "~> 0.1.1"},
+      {:dns_cluster, "~> 0.2.0"},
       {:bandit, "~> 1.5"},
       # Code quality
       {:sourceror, "~> 1.0", only: [:dev, :test], runtime: false},
