8949b1bf6f0e5b513595ae7f120f6f1120fc1f7c
Tom Clarke <tom@u2i.com>
2026-05-24 23:29:38 -0400

    Migrate demo ModalDemoLive to template do ~H + add LV tests
    Tests cover the initial demo render and the open_modal action
    flipping modal_open to true.

--- demo/lib/demo_web/live/modal_demo_live.ex
+++ demo/lib/demo_web/live/modal_demo_live.ex
@@ -17,8 +17,8 @@ defmodule DemoWeb.ModalDemoLive do
     end
   end
 
-  render fn assigns ->
-    ~L"""
+  template do
+    ~H"""
     <div class="max-w-4xl mx-auto p-6">
       <div class="flex items-center justify-between mb-6">
         <div>
---
+++ demo/test/demo_web/live/modal_demo_live_test.exs
@@ -0,0 +1,22 @@
+defmodule DemoWeb.ModalDemoLiveTest do
+  use DemoWeb.ConnCase
+  import Phoenix.LiveViewTest
+
+  describe "modal demo" do
+    test "renders the demo page with an Open Modal button", %{conn: conn} do
+      {:ok, _view, html} = live(conn, "/demos/modal")
+      assert html =~ "Modal Demo"
+      assert html =~ "Open Modal"
+      assert html =~ "simple-modal"
+    end
+
+    test "clicking Open Modal flips modal_open to true", %{conn: conn} do
+      {:ok, view, _html} = live(conn, "/demos/modal")
+
+      html = view |> element("button", "Open Modal") |> render_click()
+      # State has been bumped; modal_open is now true on the root
+      assert html =~ "modal_open"
+      assert html =~ "true"
+    end
+  end
+end
